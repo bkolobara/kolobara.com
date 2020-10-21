022a758ad607c5a9a06eb0ffd0ba7ffcbc1007fd
Bernard Kolobara <me@kolobara.com>
2020-10-21 18:36:23 +0200

    Fix mime css issues

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-s3cmd sync --delete-removed --exclude '.git/*' --exclude '.DS_Store' --exclude 'deploy.sh' --exclude '.gitignore' --exclude '.gitmodules' --exclude 'README.md' --exclude 'lunatic-page/*' --acl-public . s3://kolobara.com
-s3cmd sync --delete-removed --acl-public ./lunatic-page/build/ s3://kolobara.com/lunatic/
+s3cmd sync --no-mime-magic --guess-mime-type --delete-removed --exclude '.git/*' --exclude '.DS_Store' --exclude 'deploy.sh' --exclude '.gitignore' --exclude '.gitmodules' --exclude 'README.md' --exclude 'lunatic-page/*' --acl-public . s3://kolobara.com
+s3cmd sync --no-mime-magic --guess-mime-type --delete-removed --acl-public ./lunatic-page/build/ s3://kolobara.com/lunatic/
