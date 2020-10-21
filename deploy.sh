fea3c9f31fce190334c017acdb1a4dd44be9c05b
Bernard Kolobara <me@kolobara.com>
2020-10-21 18:07:51 +0200

    Add lunatic to subfolder

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-s3cmd sync --delete-removed --exclude '.git/*' --exclude '.DS_Store' --exclude 'deploy.sh' --exclude '.gitignore' --exclude 'README.md' --acl-public . s3://kolobara.com
+s3cmd sync --delete-removed --exclude '.git/*' --exclude '.DS_Store' --exclude 'deploy.sh' --exclude '.gitignore' --exclude '.gitmodules' --exclude 'README.md' --exclude 'lunatic-page/*' --acl-public . s3://kolobara.com
+s3cmd sync --delete-removed --acl-public ./lunatic-page/build/ s3://kolobara.com/lunatic/
